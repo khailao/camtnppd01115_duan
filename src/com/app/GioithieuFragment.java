2d752ef642669788c3d470c0d4f3169a23da2f05
camtnppd01115 <camtnppd01115>
2014-11-24 10:37:55 +0700

    camtnppd01115_duan

---
+++ src/com/app/GioithieuFragment.java
@@ -0,0 +1,24 @@
+package com.app;
+
+import edu.fpoly.edict.R;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GioithieuFragment extends Fragment {
+
+	public GioithieuFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_gioithieu, container, false);
+	}
+
+}
---
+++ src/com/app/adapter/DrawerListItem.java
@@ -0,0 +1,22 @@
+package com.app.adapter;
+
+import android.graphics.drawable.Drawable;
+
+public class DrawerListItem {
+	String title;
+	Drawable icon;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Drawable getIcon() {
+		return icon;
+	}
+	public void setIcon(Drawable icon) {
+		this.icon = icon;
+	}
+	
+	
+}
